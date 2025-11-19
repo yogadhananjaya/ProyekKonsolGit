e164c221594dc0472b3ea3e88e4e42f9c6c61477
ACER <yogadhananjaya99@gmail.com>
2025-11-19 08:26:18 +0700

    Modifikasilah program CreateStudentArchive

--- Reading.c
+++ Reading.c
@@ -1,34 +1,103 @@
 #include <stdio.h>
 
+
 typedef struct {
     int id;
     char name[25];
     float grade;
 } StudentData;
 
-void countGraduates(FILE *stdFile, int *nOver2);
+typedef StudentData StudentTab[100];
+
+void copyStudent(char *StudentFile, StudentTab students, int *n);
+void countGraduates(char *StudentFile, int *nOver2);
+void printGraduates(StudentTab students, int n);
+void exportToCsv(char *StudentFile, StudentTab students, int n);
 
 int main() {
-    FILE *studentFile;
+    StudentTab students;
+    int n;
     int numberOfGraduates;
 
-    countGraduates(studentFile, &numberOfGraduates);
-    printf("\nNumber of Graduates is %d", numberOfGraduates);
+    copyStudent("StudentFile.dat", students, &n);
+
+    countGraduates("StudentFile.dat", &numberOfGraduates);
+    printf("\nNumber of Graduates: %d\n", numberOfGraduates);
+
+    printf("\n--- Graduates List ---\n");
+    printGraduates(students, n);
+
+    exportToCsv("StudentOutput.csv", students, n);
 
     return 0;
 }
 
-void countGraduates(FILE *stdFile, int *nOver2) {
+void copyStudent(char *StudentFile, StudentTab students, int *n) {
+    FILE *stdFile = fopen(StudentFile, "rb");
+    if (stdFile == NULL) {
+        printf("Error opening %s (file not found?)\n", StudentFile);
+        return;
+    }
+
     StudentData studentRec;
+    int i = 0;
 
+    while (fread(&studentRec, sizeof(StudentData), 1, stdFile) == 1 && i < 100) {
+        students[i] = studentRec;
+        i++;
+    }
+
+    fclose(stdFile);
+    *n = i;
+}
+
+void countGraduates(char *StudentFile, int *nOver2) {
+    FILE *stdFile = fopen(StudentFile, "rb");
+    if (stdFile == NULL) {
+        printf("Error opening %s\n", StudentFile);
+        return;
+    }
+
+    StudentData studentRec;
     *nOver2 = 0;
-    stdFile = fopen("StudentFile.dat", "rb");
 
-    while (fread(&studentRec, sizeof(studentRec), 1, stdFile) == 1) {
+    while (fread(&studentRec, sizeof(StudentData), 1, stdFile) == 1) {
         if (studentRec.grade > 2.0) {
-            *nOver2 = *nOver2 + 1;
+            (*nOver2)++;
         }
     }
 
     fclose(stdFile);
+}
+
+void printGraduates(StudentTab students, int n) {
+    for (int i = 0; i < n; i++) {
+        if (students[i].grade > 2.0) {
+            printf("ID: %d\n", students[i].id);
+            printf("Name: %s\n", students[i].name);
+            printf("Grade: %.2f\n\n", students[i].grade);
+        }
+    }
+}
+
+void exportToCsv(char *StudentFile, StudentTab students, int n) {
+    FILE *file = fopen(StudentFile, "w");
+    if (file == NULL) {
+        printf("Couldn't open %s for writing\n", StudentFile);
+        return;
+    }
+
+    fprintf(file, "sep=,\n");
+    fprintf(file, "ID,Name,Grade\n");
+
+    for (int i = 0; i < n; i++) {
+        fprintf(file, "%d,%s,%.2f\n",
+                students[i].id,
+                students[i].name,
+                students[i].grade);
+    }
+
+    fclose(file);
+    printf("Data successfully saved to %s\n", StudentFile);
+    getchar();
 }
